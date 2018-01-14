4b447f76f77ba0848db9f8a67fab3be33e793c20
sillypears <sillypears@users.noreply.github.com>
2018-01-14 12:25:57 -0500

    The only commit I'll ever do most likely

---
+++ scrape.py
@@ -0,0 +1,41 @@
+#!C:\Python27\python.exe
+
+import os
+import sys
+from HTMLParser import HTMLParser
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://gamesdonequick.com/schedule"
+FILE = "agdq-2017-schedule-scrape.txt"
+
+def start_analysis(data_file):
+    with open(data_file, "r") as f:
+        soup = BeautifulSoup(f.read(), "html.parser")
+    #print soup.prettify("utf-8")
+    tds = soup.find_all("td", {"class": "visible-lg text-center"})
+    print "game, setup"
+    for td in tds:
+        try:
+            setup = td.getText().strip()
+            game = td.findPreviousSibling().findPreviousSibling().getText().strip()
+            if setup:
+                print "{},{}".format(game, setup)
+        except:
+            pass
+
+
+def main():
+    if os.path.exists(FILE):
+        start_analysis(FILE)
+    else:
+        r = requests.get(URL)
+        data = r.text
+        soup = BeautifulSoup(data, "html.parser")
+        table = soup.find_all("table")[0]
+        print len(table)
+        with open(FILE, 'w') as f:
+            f.write(table.prettify("utf-8"))
+
+if __name__ == "__main__":
+    sys.exit(main())
