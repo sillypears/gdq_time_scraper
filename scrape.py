8a1bc9d6fadb718c7705b29bbaff65029ce02ff4
sillypears <sillypears@users.noreply.github.com>
2019-01-05 21:32:18 -0500

    alright it's not terrible

--- scrape.py
+++ scrape.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import pytz
 
 URL = "https://gamesdonequick.com/schedule"
-FILE = "agdq-2018-schedule-scrape.txt"
+FILE = "agdq-2019-schedule-scrape.txt"
 GAMES = ("Overload", "Crypt of the NecroDancer: AMPLIFIED")
 tz = pytz.timezone("America/New_York")
 
@@ -16,7 +16,6 @@ def start_analysis(soup):
 
     #print soup.prettify("utf-8")
     tds = soup.find_all("td", {"class": "start-time text-right"})
-    print("game, setup")
     
     for td in tds:
         try: 
@@ -43,20 +42,6 @@ def start_analysis(soup):
             print("{} - {} by {}".format(start_time.astimezone(tz), game_name, runner_name))
         
 
-
-    # for td in tds:
-    #     print(td)
-    #     sys.exit()
-    #     try:
-    #         setup = td.getText().strip()
-    #         game = td.findPreviousSibling().findPreviousSibling().getText().strip()
-            
-    #         if setup:
-    #             print("{},{}".format(game, setup))
-    #     except:
-    #         pass
-
-
 def main():
     if os.path.exists(FILE):
         with open(FILE, "r") as f:
@@ -67,6 +52,7 @@ def main():
         soup = BeautifulSoup(data, "html.parser")
 
     start_analysis(soup)
+    os.system("pause")
 
 if __name__ == "__main__":
     sys.exit(main())
