bab8ad1b97a31c9c09c363bfbfb803a6bafa58fb
sillypears <sillypears@users.noreply.github.com>
2019-01-06 12:47:20 -0500

    Made it more dynamic and refresh every 60 seconds

--- scrape.py
+++ scrape.py
@@ -5,25 +5,17 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
 import pytz
+import time
 
 URL = "https://gamesdonequick.com/schedule"
 FILE = "agdq-2019-schedule-scrape.txt"
-GAMES = (
-    "Overload",
-    "Crypt of the NecroDancer: AMPLIFIED",
-    "Shadow of the Colossus (2018)",
-    "Castlevania: Symphony of the Night",
-    "Dead Cells",
-    "Celeste",
-    "TASBot plays mari0 (SMB1 + Portal gun)",
-    "I wanna kill the Kamilia 2",
-    "The Legend of Zelda: A Link to the Past"
-    )
+GAMES_FILE = "games-list.txt"
+SLEEP_TIME = 60
 tz = pytz.timezone("America/New_York")
 
-def start_analysis(soup):
+def start_analysis(soup, games):
     tds = soup.find_all("td", {"class": "start-time text-right"})
-    print("")
+    
     for td in tds:
         try: 
             start_time = pytz.utc.localize(datetime.fromisoformat(td.getText().strip()[:-1:]))
@@ -42,11 +34,25 @@ def start_analysis(soup):
             setup_time = td.findNextSibling().findNextSibling().findNextSibling().getText().strip()
         except:
             pass
-        if setup_time and game_name in GAMES:
-            print("{} - {} by {}".format(start_time.astimezone(tz), game_name, runner_name))
+        
+        if setup_time and game_name in games:
+            print("{} - {} by {}".format(start_time.astimezone(tz).strftime("%a @ %I:%M %p"), game_name, runner_name))
+
+    print("")
+    return 0
         
 def main():
+    os.system('cls')
     while(True):
+        GAMES = []
+        if os.path.exists(GAMES_FILE):
+            with open(GAMES_FILE, "r") as f:
+                GAMES.extend(f.read().split('\n'))
+        else:
+            GAMES = [
+                "Overload",
+                "Crypt of the NecroDancer: AMPLIFIED"
+            ]
         if os.path.exists(FILE):
             with open(FILE, "r") as f:
                 soup = BeautifulSoup(f.read(), "html.parser")
@@ -54,9 +60,12 @@ def main():
             r = requests.get(URL)
             data = r.text
             soup = BeautifulSoup(data, "html.parser")
+        os.system('cls')            
+        print("Current Time: {}".format(datetime.now().strftime("%x %X")))
+        start_analysis(soup, GAMES)
+        time.sleep(SLEEP_TIME)
+
         
-        start_analysis(soup)
-        os.system("pause")
 
 if __name__ == "__main__":
     sys.exit(main())
